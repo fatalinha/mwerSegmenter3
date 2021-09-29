d942fabc5d727d8183683ba6e14e4555826896f2
PeganovAnton <peganoff2@mail.ru>
2021-09-29 15:57:02 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- segmentBasedOnMWER.sh
+++ segmentBasedOnMWER.sh
@@ -16,7 +16,7 @@ if [ "$1" == "-h" ] ; then
          exit 0
 fi
 
-SCRIPTDIR=/home/lab/mwerSegmenter
+SCRIPTDIR="/home/${USER}/mwerSegmenter"
 sourcesgmfile=$1
 referencesgmfile=$2
 translationfile=$3
